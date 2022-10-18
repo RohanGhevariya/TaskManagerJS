da434be0b9b26fa939b8f2e2e30f967ddab4e8a4
Rohan <roh987gh@yahoo.com>
2022-10-18 02:26:30 -0400

    changes made on both sides

--- admin/admin_home.js
+++ admin/admin_home.js
@@ -223,11 +223,12 @@ loadEventListener();
 function loadEventListener() {
 document.addEventListener('DOMContentLoaded', loadTasks);
 document.addEventListener('DOMContentLoaded', loadTasksCard);
-taskList.addEventListener('click', deleteTask);
+
 }
 function addData(){
      title = document.querySelector('#tasktitle');
      Title = document.getElementById("tasktitle").value;
+     sdate = document.getElementById("startdate").value;
      date = document.getElementById("duedate").value;
      details = document.getElementById("taskdesc").value;
      assignTo = document.getElementById("assignTo").value;
@@ -243,7 +244,8 @@ function addData(){
     else
     {
         taskDetails.push({
-            "title":Title, 
+            "title":Title,
+            "sdate":sdate, 
             "date":date,
             "details":details,
             "assign":assignTo,
@@ -259,33 +261,7 @@ function addData(){
 
 } 
  
-// function myFunction(){
-//     console.log("getting data from local storage");
 
-//     var newArr = JSON.parse(window.localStorage.getItem('task'));
-    
-//     for (var i = 0; i < newArr.length; i++) {
-//     //   var savedPerson = newArr[i];
-//     //   console.log(savedPerson);
-//     //   console.log(savedPerson.title);
-//       document.getElementById("item1").innerHTML = newArr[i].title;
-//       console.log(newArr[i].title);
-      
-//     }
-// }
- 
-// function taskDetails(){
-//     var newArr = JSON.parse(window.localStorage.getItem('tasks'));
-    
-//     for (var i = 0; i < newArr.length; i++) {
-//       var savedPerson = newArr[i];
-//       document.getElementById("taskname").innerHTML = savedPerson.title;
-//       document.getElementById("taskdetails").innerHTML = savedPerson.details;
-//       document.getElementById("time").innerHTML = savedPerson.date;
-//       document.getElementById("assName").innerHTML = savedPerson.assign;
-
-// }
-// }
 
 // creating new element
 
@@ -293,14 +269,6 @@ function createNewTaskElement(task) {
     const newTask = document.createElement('li');
     newTask.className = 'collection-item';
     newTask.appendChild(document.createTextNode(task));
-    const newTaskATag = document.createElement('a');
-    newTaskATag.style.cursor = 'pointer';
-    newTaskATag.style.color = '#e01a4f';
-    newTaskATag.className = 'delete-item secondary-content';
-    newTaskATag.innerHTML = '<i class="material-icons">delete</i>';
-  
-    newTask.appendChild(newTaskATag);
-  
     taskList.appendChild(newTask);
   }
   function loadTasks() {
@@ -319,7 +287,7 @@ function createNewTaskElement(task) {
   function createTask(Title, date, details,assignTo, payRate) {
    var newTask = document.createElement('ul');
     newTask.innerHTML += `<ul class="card" id="taskDetails">
-        <li><h3>${Title}</h3></li
+        <li><h3>${Title}</h3></li>
        <li> <p>Due: ${date}</p></li>
        <li> <p>${details}</p></li>
      
@@ -339,27 +307,3 @@ function loadTasksCard() {
     createTask(task.title,task.date,task.details);
   });
 }
-// Deletes a single task from the task list
-function deleteTask(e) {
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are you sure?')) {
-      e.target.parentElement.parentElement.remove();
-
-      removeTaskFromLocalStorage(
-        e.target.parentElement.parentElement.firstChild.textContent
-      );
-    }
-  }
-}
-// Delete task from local storage
-function removeTaskFromLocalStorage(taskToDelete) {
-  let tasks;
-  tasks = JSON.parse(localStorage.getItem('Tasks'));
-  tasks.forEach(function (task) {
-    if (task === taskToDelete) {
-      tasks.splice(tasks.indexOf(task), 1);
-    }
-  });
-  localStorage.setItem('Tasks', JSON.stringify(tasks));
-}
-  
